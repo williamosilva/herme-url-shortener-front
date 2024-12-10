ac1aa9ba2faf4ff952a801c0e2843c0a1f5dd44c
William Silva <williamsilva20062005@gmail.com>
2024-12-10 12:26:59 -0300

    chore: update devDependencies and refactor main component import; enable file emission in tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import UrlShortenerHero from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <UrlShortenerHero />
   </StrictMode>
 );
